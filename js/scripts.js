cd002e5ea20db3307a132994c526f442546aa7a1
lizanderson <eanderson4@engr-134-04.eastern.ewu.edu>
2015-03-09 13:39:33 -0700

    base commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$('#sky').jQlouds({
+  wind: true
+});
+
+$('#sky').jQlouds({
+  maxClouds: 10
+});
+
+$('#sky').jQlouds({
+  maxWidth: 300,
+  maxHeight: 200,
+});
+
+$('#sky').jQlouds({
+  src: '../img/new-cloud.png',
+});
+
